da002686169b46e40c82083a7b6859577b9de07a
Watcharapong.K <147409902+Voltran1c@users.noreply.github.com>
2024-05-15 13:36:55 +0700

    Js array1 commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Exercise1
+// 1.1
+console.log(["A", "B", "C", "D", "E", "F", "G"].length);
+
+// 1.2
+const myPlanet = ["Earth", "Pluto", "Venus", "Neptune", "Sun"];
+console.log(myPlanet);
+myPlanet.forEach(members => console.log(members));
+
+// 1.3
+const myArr = [1, 2, 'One', true];
+console.log(myArr);
+myArr.forEach(member => console.log(member));
